433dce27c6b562b20716d4007c49cf65920e4e80
Roman Caro <roman.caro@etu.unilim.fr>
2026-05-04 15:31:25 +0200

    Add class Rack

---
+++ src/main/java/latice/model/Rack.java
@@ -0,0 +1,24 @@
+package latice.model;
+
+public class Rack {
+	
+	private List<Tile> tiles;
+	private int maxSize = 5;
+	
+	public void addTile(Tile t) {
+		//TODO
+	}
+	
+	public void removeTile(Tile t) {
+		//TODO
+	}
+	
+	public boolean isEmpty() {
+		//TODO
+	}
+	
+	public int size() {
+		//TODO
+	}
+
+}
