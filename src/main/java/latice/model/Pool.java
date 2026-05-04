f6db68a038d9b0f6a2b0e3bc32c98b4c6a560d14
Oscar Cypryk <oscar.cypryk@etu.unilim.fr>
2026-05-04 15:32:45 +0200

    WIP Pool class

---
+++ src/main/java/latice/model/Pool.java
@@ -0,0 +1,27 @@
+package latice.model;
+
+import java.util.List;
+
+public class Pool extends Tile {
+	
+	private List<Tile> tiles;
+	
+	
+	public List<Tile> shuffle() {
+		return tiles; //TODO shuffle method
+	}
+	
+	public Tile draw() {
+		//TODO draw method 
+	}
+	
+	public int size() {
+		return 0; //TODO size method
+	}
+	
+	public boolean isEmpty() {
+		//TODO isEmpty method
+	}
+	
+	
+}
