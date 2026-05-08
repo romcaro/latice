ce13abee39769cd80dd474f1a689331c0c22b6ce
Oscar Cypryk <oscar.cypryk@etu.unilim.fr>
2026-05-08 17:36:54 +0200

    Add class Cell + Add GameBoard methods

---
+++ src/main/java/latice/model/Cell.java
@@ -0,0 +1,14 @@
+package latice.model;
+
+public class Cell {
+
+    private Position position;
+
+    public Cell(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+}
--- src/main/java/latice/model/GameBoard.java
+++ src/main/java/latice/model/GameBoard.java
@@ -1,5 +1,25 @@
 package latice.model;
 
-public class GameBoard {
+import java.util.HashMap;
+import java.util.Map;
 
+public class GameBoard {
+	
+	private Map<Position, Cell> cells = new HashMap<>();
+
+    public GameBoard(int width, int height) {
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                Position position = new Position(x, y);
+
+                cells.put(position, new Cell(position));
+            }
+        }
+    }
+
+    public Cell getCase(int x, int y) {
+        return cells.get(new Position(x, y));
+    }
 }
