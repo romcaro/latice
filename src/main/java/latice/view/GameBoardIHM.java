1da62dc2c3b9691d644fe7cde3ceff7d89ed753e
Roman Caro <roman.caro@etu.unilim.fr>
2026-05-11 09:24:15 +0200

    New branch IHM

---
+++ src/main/java/latice/view/GameBoardIHM.java
@@ -0,0 +1,12 @@
+package latice.view;
+
+import Application;
+
+public class GameBoardIHM extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
