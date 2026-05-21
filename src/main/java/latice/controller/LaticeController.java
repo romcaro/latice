4f6bf6dd2db61da80bcee4687e07b000318eb09a
Oscar Cypryk <oscar.cypryk@etu.unilim.fr>
2026-05-21 09:43:12 +0200

    WIP Bugfix pom.xml + rename GameBoardIHM into GameBoardFX

--- src/main/java/latice/controller/LaticeController.java
+++ src/main/java/latice/controller/LaticeController.java
@@ -1,6 +1,8 @@
 package latice.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import latice.model.Position;
 import latice.model.Square;
@@ -10,8 +12,25 @@ public class LaticeController {
     @FXML
     private GridPane gridPane;
     
+    public static Image loadImage() {
+        return new Image(LaticeController.class.getResource("latice/assets/bg_sun.png").toExternalForm());
+    }
+    
     private void setImageView(GridPane gridPane, int width, int height) {
-    	
+    	for(int i=0; i < height; i++) {
+    		for(int j=0; j < width; j++) {
+    			
+    			Square square = new Square(new Position(i,j));
+    			
+    			ImageView imageView = new ImageView(loadImage());
+    			
+    			gridPane.add(imageView, i, j);
+    			
+    			
+    			
+    			
+    		}
+    	}
     }
     
 
