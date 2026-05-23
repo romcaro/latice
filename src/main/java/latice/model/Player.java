3f509394cd209a31b50021dfcf846333eed46e7f
Théo Nidos <theo.nidos@etu.unilim.fr>
2026-05-23 17:15:23 +0200

    Add class Player

---
+++ src/main/java/latice/model/Player.java
@@ -0,0 +1,52 @@
+package latice.model;
+
+public class Player {
+	
+	private String name;
+	private Pool pool;
+	private Rack rack;
+	private int score;
+	private int tilesPlayed;
+
+	public Player(String name, Pool pool) {
+		this.name = name;
+		this.pool = pool;
+		this.rack = new Rack();
+		this.score = 0;
+		this.tilesPlayed = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void addScore(int points) {
+		this.score += points;
+	}
+
+	public Rack getRack() {
+		return rack;
+	}
+	
+	public Pool getPool() {
+		return pool;
+	}
+	
+	public int getTilesPlayed() {
+		return tilesPlayed;
+	}
+	
+	public void addTilesPlayed() {
+		this.tilesPlayed++;
+	}
+	
+	@Override
+	public String toString() {
+		return "Player{name='" + name + "', score=" + score + ", tilesPlayed=" + tilesPlayed + "}";
+	}
+
+}
