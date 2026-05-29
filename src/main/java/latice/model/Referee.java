2d74b058adbacd6dcb9adb9b88a4d3d3bb8b4ffd
Roman Caro <roman.caro@etu.unilim.fr>
2026-05-29 09:04:11 +0200

    Revert "Merge DnD to master"
    This reverts commit e4d6529c1c29e486b9c92b49c4dd6609c3032ef1, reversing
    changes made to 45b3a64e9a092665d5dfa87274c8b2003a7926af.

--- src/main/java/latice/model/Referee.java
+++ src/main/java/latice/model/Referee.java
@@ -4,10 +4,12 @@ public class Referee {
 	
 	private int cycleCount;
 	private GameBoard gameBoard;
+	private Boolean gameOver;
 	
 	public Referee(GameBoard gameBoard) {
 		this.gameBoard = gameBoard;
 		this.cycleCount = 0;
+		this.gameOver = false;
 	}
 
 	public int getCycleCount() {
@@ -21,30 +23,5 @@ public class Referee {
 	public GameBoard getGameBoard() {
 		return gameBoard;
 	}
-	
-	public boolean isValidMove(GameBoard board,Tile tile,int col,int row) {
-
-		Square square = board.getSquare(col, row);
-		
-		// case déjà occupée
-		if (square.isOccupied()) {
-		return false;
-		}
-		
-		//premiere tuile sur une lune 
-		if (board.isEmpty() && square.getType() != SquareType.MOON) {
-			return false;
-			
-		}
-		
-		if (!board.isEmpty() && !board.allNeighborsMatch(square, tile))
-			return false;
-
-		return true;
-		}
-	
-		
-	
-	
 	
 }
